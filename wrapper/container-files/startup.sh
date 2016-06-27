afc870cbb7c5d4530ea7840855086d9f329b1815
Kit Yan Choi <kchoi@enthought.com>
2016-06-27 14:46:31 +0100

    use URL_ID if given, otherwise use container id as url

--- wrapper/container-files/startup.sh
+++ wrapper/container-files/startup.sh
@@ -25,15 +25,18 @@ mkdir -p /var/run/sshd
 # This is the only effective way I found to obtain the full container id from
 # within the container. We can't have it passed from the outside, or we have a
 # chicken-egg problem, and --cidfile is unsupported by dockerpy
-_tmp=`cat /proc/self/cgroup | grep ":cpu:" | cut -d: -f 3`
-export CONTAINER_ID="`basename $_tmp`"
+_tmp=`cat /proc/self/cgroup | grep ":cpu" | head -1 | cut -d: -f 3`
+
+# If URL_ID is given as an environment variable, use it,
+# Otherwise, use the container id from $_tmp
+export URL_ID="`(test $URL_ID && echo $URL_ID) || basename $_tmp`"
 
 # Create the user
 id -u $USER &>/dev/null || useradd --create-home --shell /bin/bash --user-group $USER
 echo "$USER:$USER" | chpasswd
 
 # Parse the templates and put their result in the appropriate places
-cat /templates/nginx.conf.template | envsubst '$JPY_BASE_USER_URL $CONTAINER_ID' > /etc/nginx/sites-enabled/default
+cat /templates/nginx.conf.template | envsubst '$JPY_BASE_USER_URL $URL_ID' > /etc/nginx/sites-enabled/default
 cat /templates/supervisord.conf.template | envsubst '$USER' > /etc/supervisor/conf.d/supervisord.conf
 
 # Start the services
