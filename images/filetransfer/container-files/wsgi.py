a78914676da11bcbff6292ad5566022d4fb69027
Stefano Borini <sborini@enthought.com>
2016-09-08 10:52:45 +0100

    Fixes WSGI forwarding so that the application can correctly remap urls

--- images/filetransfer/container-files/wsgi.py
+++ images/filetransfer/container-files/wsgi.py
@@ -7,17 +7,6 @@ class ReverseProxied(object):
     front-end server to add these headers, to let you quietly bind
     this to a URL other than / and to an HTTP scheme that is
     different than what is used locally.
-
-    In nginx:
-    location /myprefix {
-        proxy_pass http://192.168.0.1:5001;
-        proxy_set_header Host $host;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Scheme $scheme;
-        proxy_set_header X-Script-Name /myprefix;
-        }
-
-    :param app: the WSGI application
     '''
     def __init__(self, app):
         self.app = app
@@ -30,7 +19,7 @@ class ReverseProxied(object):
             if path_info.startswith(script_name):
                 environ['PATH_INFO'] = path_info[len(script_name):]
 
-        scheme = environ.get('HTTP_X_SCHEME', '')
+        scheme = environ.get('HTTP_X_FORWARDED_PROTO', '')
         if scheme:
             environ['wsgi.url_scheme'] = scheme
         return self.app(environ, start_response)
