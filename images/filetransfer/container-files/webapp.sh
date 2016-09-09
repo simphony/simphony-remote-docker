66352f324f3d458c3ed6712fbdc09835eaa36080
Stefano Borini <sborini@enthought.com>
2016-09-09 18:20:34 +0100

    Works but with the incorrect initial path. Better than nothing

--- images/filetransfer/container-files/webapp.sh
+++ images/filetransfer/container-files/webapp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export HOME="/home/$USER"
+service php5-fpm start
 cat /webapp/nginx.conf.template | envsubst '$JPY_BASE_USER_URL $URL_ID' > /webapp/nginx.conf
 nginx -c /webapp/nginx.conf
