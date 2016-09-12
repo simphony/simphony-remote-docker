e5e8c80bd5007dc80a694f6136d69c30e9db082e
Stefano Borini <sborini@enthought.com>
2016-09-12 10:15:25 +0100

    Working deployment with the exception of incorrect workspace permissions

--- images/filetransfer/container-files/webapp.sh
+++ images/filetransfer/container-files/webapp.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export HOME="/home/$USER"
+cat /templates/php-fpm-pool.conf.template | envsubst '$USER' > /etc/php5/fpm/pool.d/www.conf
 service php5-fpm start
 cat /webapp/nginx.conf.template | envsubst '$JPY_BASE_USER_URL $URL_ID' > /webapp/nginx.conf
 nginx -c /webapp/nginx.conf
