e39731293b084b31284a232d951fe0e216632ad9
Stefano Borini <sborini@enthought.com>
2017-02-17 19:06:00 +0000

    Adapted production app to new layout

--- scripts/create_production_app.sh
+++ scripts/create_production_app.sh
@@ -7,10 +7,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LABEL_DOMAIN=eu.simphony-project
 
-. functions.sh
+. $DIR/functions.sh
 
 display_help() {
-  echo "Usage: $0 build.conf"
+  echo "Usage: $0 path/to/build.conf"
   echo
   echo "Creates a production directory containing the docker context for DockerHub auto build"
   echo
@@ -24,61 +24,56 @@ if [ -z "$1" ]; then
 fi
 
 config_file=$1
+operating_dir=`dirname $config_file`/
 
 # The directory that contains all base images
-extract_key "$DIR/$config_file" "app_images_dir"
+extract_key "$config_file" "app_dir"
 if [ -z "$RESULT" ]; then
-    echo "Need app_images_dir in config file"
+    echo "Need app_dir in config file"
     display_help
     exit 1
 fi
-app_images_dir=$DIR/${RESULT%/}
+app_dir=$operating_dir/${RESULT%/}
 
-extract_key "$DIR/$config_file" "tag"
+extract_key "$config_file" "base_tag"
 if [ -z "$RESULT" ]; then
-    echo "Need tag in config file"
+    echo "Need base_tag in config file"
     display_help
     exit 1
 fi
 tag=$RESULT
 
 # production directory
-extract_key "$DIR/$config_file" "production_dir"
+extract_key "$config_file" "production_dir"
 if [ -z "$RESULT" ]; then
     echo "Need production_dir in config file"
     display_help
     exit 1
 fi
-production_dir=$DIR/${RESULT%/}
+production_dir=$operating_dir/${RESULT%/}
+image_name=`basename $app_dir`
 
-# Construct docker context for production
-for image in `ls -d $app_images_dir/*/`; do
-    image_name=`basename $image`
+# One sub-directory for each image
+echo "Removing "$production_dir/$image_name
+rm -rf $production_dir/$image_name
 
-    # One sub-directory for each image
-    echo "Removing "$production_dir/$image_name
-    rm -rf $production_dir/$image_name
+echo "Creating "$production_dir/$image_name
+mkdir -p $production_dir/$image_name
+# Copy files from the image directory to the production sub-directory
+echo "Copying files from " $app_dir " to " $production_dir/$image_name
+rsync -a --exclude='*~' $app_dir/* $production_dir/$image_name/
 
-    echo "Creating "$production_dir/$image_name
-    mkdir -p $production_dir/$image_name
+# Replace the tag in the docker file FROM entry
+sed 's/^FROM \([^:]*\)/FROM \1:'$base_tag'/g' $production_dir/$image_name/Dockerfile > $production_dir/$image_name/Dockerfile.build
 
-    # Copy files from the image directory to the production sub-directory
-    echo "Copying files from " $app_images_dir/$image_name
-    rsync -a --exclude='*~' $app_images_dir/$image_name/* $production_dir/$image_name/
+# if there's an icon, base encode it and use it.
+if [ -e $production_dir/$image_name/icon_128.png ]; then
+    b64encode $production_dir/$image_name/icon_128.png
+    echo "LABEL ${LABEL_DOMAIN}.docker.icon_128=\"${RESULT}\"" >>$production_dir/$image_name/Dockerfile.build
+fi
 
-    # Replace the tag in the docker file FROM entry
-    sed 's/^FROM \([^:]*\)/FROM \1:'$tag'/g' $production_dir/$image_name/Dockerfile > $production_dir/$image_name/Dockerfile.build
-
-    # if there's an icon, base encode it and use it.
-    if [ -e $production_dir/$image_name/icon_128.png ]; then
-        b64encode $production_dir/$image_name/icon_128.png
-        echo "LABEL ${LABEL_DOMAIN}.docker.icon_128=\"${RESULT}\"" >>$production_dir/$image_name/Dockerfile.build
-    fi
-
-    mv $production_dir/$image_name/Dockerfile.build $production_dir/$image_name/Dockerfile
-done
+mv $production_dir/$image_name/Dockerfile.build $production_dir/$image_name/Dockerfile
 
 echo "***********************************************************************"
-echo "Now all the files for Docker build are ready in $production_dir"
-echo "Please push this directory to an orphan branch"
+echo "Now all the files for Docker build are in $production_dir/$image_name"
 echo "***********************************************************************"
